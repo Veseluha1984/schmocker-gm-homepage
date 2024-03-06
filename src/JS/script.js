6f118464bd9c3ffc42d09acd1801f36ff95caac5
Yauheni <veseluha1984@icloud.com>
2024-03-06 18:22:33 +0100

    storis is set

---
+++ src/JS/script.js
@@ -0,0 +1,14 @@
+<!--  <script>window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    var subheaderFarbe = document.querySelector('.subheader .farbe');
+    if (window.pageYOffset > 100) { // Замените 50 на значение, после которого вы хотите начать прокрутку
+        subheaderFarbe.style.position = 'fixed';
+        subheaderFarbe.style.top = '0';
+    } else {
+        subheaderFarbe.style.position = 'static';
+    }
+}
+</script> 
+
+</section>
